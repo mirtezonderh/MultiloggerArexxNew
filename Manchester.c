251277344b353826fb3b55b4ec8ce1d265b02ca4
mirte <mirteholmm@gmail.com>
2022-06-01 16:18:15 +0200

    Added better variable names

--- Manchester.c
+++ Manchester.c
@@ -3,7 +3,7 @@
 #include<string.h>
 #include "Manchester.h"
 
-uint8_t test[4];
+uint8_t decodedBit[4];
 
 
 uint8_t manDecode(uint8_t d)
@@ -19,26 +19,26 @@ uint8_t manDecode(uint8_t d)
 
             if(temp == 1) // if the shifted value = 00000001 it is 1 in binary which is 0 in manchester and vice versa.
                 {
-                    test[count] = 0;
+                    decodedBit[count] = 0;
 
                 }
             else
                 {
-                    test[count] = 1;
+                    decodedBit[count] = 1;
                 }
 
         count++; // add 1 to count
     }
 
 
-    uint8_t decoded = 0;
+    uint8_t decodedByte = 0;
 
     for(i = 0; i<4; i++) // from array to uint
         {
-            decoded = decoded | test[i];
-            decoded = decoded << 1;
+            decodedByte = decodedByte | decodedBit[i];
+            decodedByte = decodedByte << 1;
         }
-decoded = decoded >> 1;
-    return(decoded); // after it's done return the test array.
+decodedByte = decodedByte >> 1;
+    return(decodedByte); // after it's done return the test array.
 
  }
